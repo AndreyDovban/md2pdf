63d83009b96a4e4a3bb61f196f7527da3fe5d8a6
Довбан Андрей Сергеевич <dovban@mdis.ru>
2025-10-15 14:44:53 +0300

    Реализован скрипт добавления нижних и верхних колонтитулов, Закончена работа с обложкой.

--- Dockerfile
+++ Dockerfile
@@ -10,26 +10,6 @@ RUN apt -y install wget
 RUN apt -y install pandoc
 RUN wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
 RUN apt -y install ./wkhtmltox_0.12.6.1-3.jammy_amd64.deb
-RUN  ln -s /usr/bin/wkhtmltopdf /usr/local/bin/html2pdf
 RUN mkdir /root/.fonts
 COPY fonts/ /root/.fonts
 RUN fc-cache -f -v
-RUN apt -y install pdftk
-
-
-RUN apt -y install qpdf
-# RUN fc-cache -f -v  qpdf
-
-# RUN mv ./wkhtmltox/bin/wkhtmltoimage /usr/local/bin/
-# RUN mv ./wkhtmltox/bin/wkhtmltopdf /usr/local/bin/
-
-
-# RUN  apt -y install ./wkhtmltopdf_0.12.6-2build2_amd64.deb
-# RUN DEBIAN_FRONTEND=noninteractivе apt -y install texlive-xetex
-# RUN apt -y install texlive-lang-cyrillic
-
-# COPY xetex-install.sh /tmp/
-# RUN /tmp/xetex-install.sh
-# texlive-xetex
-# fc-cache -f -v
-# -V 'mainfont:Montserrat'
--- script.sh
+++ script.sh
@@ -1,9 +1,50 @@
-./md2html.sh $1
+docker run \
+    -it \
+    --rm \
+    --user andrey \
+    -v $(pwd):/opt/app \
+    ubuntu-md2pdf \
+    pandoc \
+        $1.md \
+        -s \
+        -c ./modules/styles.css \
+        -o $1.html
 
-./html2coverpdf.sh 72 $2
 
-./html2pdf.sh 72 $3
 
-./add-cover.sh $2 $3 $4
 
-chown andrey:andrey $4.pdf
+sed -i '/<body>/r ./modules/logotip.html' ./$1.html
+
+docker run \
+    -it \
+    --rm \
+    --user andrey \
+    -v $(pwd):/opt/app \
+    ubuntu-md2pdf \
+    wkhtmltopdf \
+        --dpi 72 \
+        --zoom 1.25 \
+        --enable-toc-back-links \
+        --disable-smart-shrinking \
+        --encoding UTF-8 \
+        --enable-local-file-access \
+        --footer-spacing  0 \
+        --footer-html ./modules/footer.html \
+        --header-html ./modules/header.html \
+        --margin-bottom 36px \
+        --margin-left 0px \
+        --margin-top 24px \
+        --margin-right 0px \
+        ./modules/cover.html \
+        $1.html \
+        $1.pdf
+
+
+
+        # --footer-html ./modules/footer.html \
+        # --footer-right "[page]/[topage]" \
+        # --header-html ./header.html \
+    # -e XDG_RUNTIME_DIR="/run/user/1000" \
+
+
+ 
