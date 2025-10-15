45eeb1485a40cd074e4bb2a19920add652da2d73
Довбан Андрей Сергеевич <dovban@mdis.ru>
2025-10-15 18:27:32 +0300

    Добавлен парсинг метаданных и добавление их в обложку и шапку документа

--- script.sh
+++ script.sh
@@ -1,3 +1,43 @@
+# Дефолтные значения переменных
+title=Заголовок
+product=Продукт
+version="версия продукта"
+updated_at="последнее обновление"
+
+# Плучение имени файла без расширений и пути
+file_path=$1
+file_name="${file_path%.*}"
+file_name="${file_name##*/}"
+echo $file_name
+
+
+# Парсинг метаданных целевого markdown файла
+while IFS= read -r line; do
+  if [[ "$line" =~ "product:"|"title:"|"version:"|"updated_at:"  ]]; then
+    case "$line" in
+       *product*)
+        product="${line##*product\: }"
+        echo "Product: ${product}" # Отладочный вывод
+        ;;
+      *title*)
+        title="${line##*title\: }"
+        echo "Title: ${title}" # Отладочный вывод
+        ;;
+      *version*)
+        version="${line##*version\: }"
+        echo "Version: ${version}" # Отладочный вывод
+        ;;
+      *updated_at*)
+        updated_at="${line##*updated_at\: }"
+        echo "Updated at: ${updated_at}" # Отладочный вывод
+        ;;
+    esac
+  fi
+done < $1
+
+ 
+
+# Создание сосновного html файла из целевого markdown файла
 docker run \
     -it \
     --rm \
@@ -5,16 +45,30 @@ docker run \
     -v $(pwd):/opt/app \
     ubuntu-md2pdf \
     pandoc \
-        $1.md \
+        $file_name.md \
         -s \
         -c ./modules/styles.css \
-        -o $1.html
+        -o $file_name.html
 
 
+# Создание обложки документа, подставление нужных значений 
+cp ./modules/cover.html ./modules/cover_temp.html
+sed -i "s/TITLE/${title}/" ./modules/cover_temp.html
+sed -i "s/PRODUCT/${product}/" ./modules/cover_temp.html
+sed -i "s/VERSION/${version}/" ./modules/cover_temp.html
+sed -i "s/UPDATED_AT/${updated_at}/" ./modules/cover_temp.html
 
+# Создание шапки документа, подставление нужных значений 
+cp ./modules/logotip.html ./modules/logotip_temp.html
+sed -i "s/TITLE/${title}/" ./modules/logotip_temp.html
+sed -i "s/PRODUCT/${product}/" ./modules/logotip_temp.html
+sed -i "s/VERSION/${version}/" ./modules/logotip_temp.html
+sed -i "s/UPDATED_AT/${updated_at}/" ./modules/logotip_temp.html
 
-sed -i '/<body>/r ./modules/logotip.html' ./$1.html
+# Добавление блока с логотипом с основной html фалй
+sed -i '/<body>/r ./modules/logotip_temp.html' ./$file_name.html
 
+# Создание итогового pdf файла из html файла боложки и основного html файла
 docker run \
     -it \
     --rm \
@@ -29,22 +83,25 @@ docker run \
         --encoding UTF-8 \
         --enable-local-file-access \
         --footer-spacing  0 \
+        --header-spacing  -1 \
         --footer-html ./modules/footer.html \
         --header-html ./modules/header.html \
         --margin-bottom 36px \
         --margin-left 0px \
         --margin-top 24px \
         --margin-right 0px \
-        ./modules/cover.html \
-        $1.html \
-        $1.pdf
+        ./modules/cover_temp.html \
+        $file_name.html \
+        $file_name.pdf
+
+
+# Удаление временных файлов
+rm $file_name.html
+rm ./modules/cover_temp.html
+rm ./modules/logotip_temp.html
 
 
 
-        # --footer-html ./modules/footer.html \
-        # --footer-right "[page]/[topage]" \
-        # --header-html ./header.html \
-    # -e XDG_RUNTIME_DIR="/run/user/1000" \
 
 
  
